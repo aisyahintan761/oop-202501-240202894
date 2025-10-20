72dfe903aab38e362060f4228b671fdb85ae7002
aisyahintan761 <aisyahintan382@gmail.com>
2025-10-20 17:02:00 +0700

    week1-setup-hello-pos

---
+++ praktikum/week1-setup-hello-pos/src/main/java/com/upb/agripos/HelloFunctional.java
@@ -0,0 +1,13 @@
+package main.java.com.upb.agripos;
+
+// HelloFunctional.java
+import java.util.function.BiConsumer;
+
+public class HelloFunctional {
+    public static void main(String[] args) {
+        BiConsumer<String, String> introduce =
+            (nim, name) -> System.out.println("Hello World, I am " + name + " - " + nim);
+
+        introduce.accept("240202894", "Aisyah");
+    }
+}
---
+++ praktikum/week1-setup-hello-pos/src/main/java/com/upb/agripos/HelloProcedural.java
@@ -0,0 +1,12 @@
+package main.java.com.upb.agripos;
+
+// HelloProcedural.java
+public class HelloProcedural {
+    public static void main(String[] args) {
+        String nim = "240202894";
+        String name = "Aisyah";
+
+        System.out.println("Hello World, I am " + name + " - " + nim);
+    }
+}
+
